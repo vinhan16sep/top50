981e06089706c4e78e83b2ea9f0b935e9b67d55b
vinhan16sep <vinhan16sep@gmail.com>
2019-07-19 01:29:45 +0700

    done

--- application/views/auth/email/activate.tpl.php
+++ application/views/auth/email/activate.tpl.php
@@ -3,7 +3,11 @@
 	<strong>Kính gửi Quý công ty!</strong>
 	<p>Cảm ơn Quý Công ty đã đăng ký tham gia Chương trình 50+10 Doanh nghiệp CNTT hàng đầu Việt Nam 2019. Quý Công ty đã sử dụng email: <?php echo $identity ?> để đăng ký tài khoản tham gia chương trình.</p>
 	<p><?php echo sprintf(lang('email_activate_subheading'), anchor('client/user/activate/'. $id .'/'. $activation, 'đây'));?> để kích hoạt tài khoản và khai hồ sơ.</p>
-	<p>Trường hợp cần hỗ trợ đăng nhập được vui lòng liên hệ: <strong>Anh Mạc Công Minh</strong>, mobile: 0936 136 696/024 3577 2336, email: minhmc@vinasa.org.vn để được hỗ trợ</p>
+    <p>Nếu cần hỗ trợ, vui lòng liên hệ: Anh Nguyễn Thế Anh</p>
+    <p>Hiệp hội Phần mềm và Dịch vụ CNTT Việt Nam</p>
+    <p>Tầng 11, tòa nhà Cung Trí thức thành phố, Số 1 Tôn Thất Thuyết, Cầu Giấy, Hà Nội</p>
+    <p>Email: anhnt@vinasa.org.vn</p>
+    <p>Mobile: 091 319 66 99/02435772336</p>
 	<br>
 	<br>
 	<br>
