1ac55c22c3dcd8e4067b9b83df20ef552fb24a8f
quyenvkbn <12quyen12@gmail.com>
2019-07-23 11:16:32 +0700

    update message register

--- application/controllers/client/User.php
+++ application/controllers/client/User.php
@@ -101,8 +101,7 @@ class User extends MY_Controller {
             }
             $detail = $this->users_model->fetch_by_id($result);
             if($result){
-                $this->session->set_flashdata('register_success', 'Cảm ơn Qúy Công ty đã đăng ký tham gia Chương trình Danh hiệu Sao Khuê 2019.
-                                                Vui lòng truy cập email để kích hoạt tài khoản và khai hồ sơ.');
+                $this->session->set_flashdata('register_success', 'Cảm ơn Qúy Công ty đã đăng ký tham gia Chương trình 50+10 doanh nghiệp CNTT hàng đầu Việt Nam 2019. Vui lòng truy cập email để kích hoạt tài khoản và khai hồ sơ.');
                 redirect('client/user/login', 'refresh');
                 $this->ion_auth->login($username, $password, false);
                 redirect('client', 'refresh');
